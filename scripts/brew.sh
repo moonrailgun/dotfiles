71db3b7d03d78d2c2594aea9c933aff1e267d56c
moonrailgun <moonrailgun@gmail.com>
2021-07-19 15:49:24 +0800

    init

---
+++ scripts/brew.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+brew install htop
+brew install iftop
+brew install tmux
+brew install fzf
---
+++ scripts/ohmyzsh.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
